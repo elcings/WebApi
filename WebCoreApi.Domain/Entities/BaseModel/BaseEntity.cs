c4ae1f453bd6a1c653f121d8e8b6e246e41b7ee7
Elcin Aliyev <elcinaliyevgs@gmail.com>
2020-03-12 16:52:48 +0400

    WebApi

---
+++ WebCoreApi.Domain/Entities/BaseModel/BaseEntity.cs
@@ -0,0 +1,10 @@
+﻿
+using System;
+
+namespace WebCoreApi.Domain.Entities.BaseModel
+{
+    public abstract class BaseEntity
+    {
+        public Guid Id { get; set; }
+    }
+}
---
+++ WebCoreApi.Domain/Entities/Catalog/Product.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using WebCoreApi.Domain.Entities.BaseModel;
+
+namespace WebCoreApi.Domain.Entities.Catalog
+{
+    public class Product: BaseEntity
+    {
+        public string ProductName { get; set; }
+        public double Price { get; set; }
+        public string Description { get; set; }
+    }
+}
